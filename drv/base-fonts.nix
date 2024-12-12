8cc3e4d1b4b5f5fbcad93b0b6f6db143b8eb69c0
Haam-z <hamzazarouk@gmail.com>
2024-12-12 19:27:49 +0100

    Fixed the fetcher

--- drv/base-fonts.nix
+++ drv/base-fonts.nix
@@ -1,11 +1,12 @@
 { pkgs }: pkgs.stdenv.mkDerivation rec {
   name = "base-fonts";
   src = pkgs.fetchFromGitHub {
-    owner = "selynaia";
+    owner = "Selynaia";
     repo = "fonts";
     rev = "afb836b1105bdecf025c90f4636edb1779685c99";
     hash = "sha256-b8k/y2WtF7O8YKekOkGi2JviYy28GMRCvT6VxhLR+/c=";
-    #hash = "sha256-FBHjxZRlBPMcwzwIUMWYU3nWeyYyYPZMvYWD6FJYzYU=";
+    fetchSubmodules = false;
+    deepClone = false;
     sparseCheckout = [
       "base-fonts"
     ];
